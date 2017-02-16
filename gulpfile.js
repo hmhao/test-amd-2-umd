f19098bf6356ae71e21c3389480ffa34e45b6e2f
hmh <215795583@qq.com>
2017-02-16 14:12:27 +0800

    add annotation for the custom dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,14 @@ gulp.task('umd', function() {
     gulp.src(src)
         .pipe(umdwrap({
             dependencies: function (file) {
+                /**
+                 *  \s*define[^\(]*?\( ：define前面可能有换行空格，匹配define函数非贪婪的直到左小括号
+                 *  [^\[]*?(\[[^\]]*?\])? ： 非贪婪的匹配到左中括号，中括号里面的内容，右中括号，该中括号可能会没有，即define没有依赖
+                 *  \s*,?\s*? ： 匹配到function前面，如果前面的中括号没有那么逗号也可能没有
+                 *  function\s*\(([^\)]*)?\)[^\{]*?\{) ： 匹配function以及其参数直到函数体前的左大括号，参数可能没有
+                 *  [\w\W]* ： 匹配function函数体直到右大括号前
+                 *  (\}\s*\)\s*;*\s*$ ： 匹配function函数体右大括号以及define函数的右小括号，define函数后可能有分号和换行空格
+                 */
                 var match = file.contents.toString().match(/(\s*define[^\(]*?\([^\[]*?(\[[^\]]*?\])?\s*,?\s*?function\s*\(([^\)]*)?\)[^\{]*?\{)[\w\W]*(\}\s*\)\s*;*\s*)$/);
                 if(match){
                     file.contents = file.contents.slice(match[1].length, -(match[4].length));
